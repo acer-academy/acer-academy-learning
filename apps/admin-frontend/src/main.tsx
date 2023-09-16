61b5179d1c5f306f90d2c56b43a982d728b496de
ydymegan <meganyee1404@gmail.com>
2023-09-16 22:04:03 +0800

    Add browser router

--- apps/admin-frontend/src/main.tsx
+++ apps/admin-frontend/src/main.tsx
@@ -1,15 +1,18 @@
-import * as React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
-import * as ReactDOM from 'react-dom/client'
+import * as React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import * as ReactDOM from 'react-dom/client';
 import App from './app/app';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>,
-)
+);
