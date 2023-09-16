486a352c5e2fe1433de62b46e7e471e34f76472c
cjunhong99 <cjunhongg99@gmail.com>
2023-09-16 16:52:36 +0800

    fix(frontend): add BrowserRouter to all 3 frontends

--- apps/admin-frontend/src/main.tsx
+++ apps/admin-frontend/src/main.tsx
@@ -1,12 +1,16 @@
-import * as React from 'react';
+import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './app/app';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
 );
