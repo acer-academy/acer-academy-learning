be98ea3903beb4b57dc15b4731ced269444dda05
keithcharlesc <keithcharleschan@gmail.com>
2023-09-16 11:04:48 +0800

    Configured main.tsx of 3 frontends to work with Chakra UI

--- apps/admin-frontend/src/main.tsx
+++ apps/admin-frontend/src/main.tsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
-
+import * as React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import * as ReactDOM from 'react-dom/client'
 import App from './app/app';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
+)
--- apps/student-frontend/src/main.tsx
+++ apps/student-frontend/src/main.tsx
@@ -1,16 +1,15 @@
-import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-
+import * as React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import * as ReactDOM from 'react-dom/client'
 import App from './app/app';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <StrictMode>
-    <BrowserRouter>
+  <React.StrictMode>
+    <ChakraProvider>
       <App />
-    </BrowserRouter>
-  </StrictMode>
-);
+    </ChakraProvider>
+  </React.StrictMode>,
+)
