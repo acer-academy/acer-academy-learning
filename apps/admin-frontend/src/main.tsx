c2bb7e1cac9cc0bae1e4266ae22d7aa6a345190e
cjunhong99 <cjunhongg99@gmail.com>
2023-09-16 16:46:36 +0800

    fix: rewrite chakra components to antd

--- apps/admin-frontend/src/main.tsx
+++ apps/admin-frontend/src/main.tsx
@@ -1,15 +1,12 @@
-import * as React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
-import * as ReactDOM from 'react-dom/client'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
 import App from './app/app';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <App />
   </React.StrictMode>,
-)
+);
