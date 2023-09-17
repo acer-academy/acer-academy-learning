38755b0aa098f3033b5915118f9355b2a942a20a
keithcharlesc <keithcharleschan@gmail.com>
2023-09-17 15:59:19 +0800

    TailwindCSS Integration with Ant Design

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+// postcss.config.js
+module.exports = {
+  plugins: {
+    'tailwindcss': {},
+    'autoprefixer': {},
+    'postcss-antd-fixes': {
+      // Support multiple prefixes, if you do not custom antd class name prefix, it's not necessary option.
+      prefixes: ['vp-antd', 'ant'],
+    },
+  },
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './apps/admin-frontend/src/**/*.{html,js,ts,jsx,tsx}', 
+    './apps/student-frontend/src/**/*.{html,js,ts,jsx,tsx}', 
+    './apps/teacher-frontend/src/**/*.{html,js,ts,jsx,tsx}', 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  // ...
+};
