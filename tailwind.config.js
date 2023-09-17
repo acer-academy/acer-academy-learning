b8323a7143f2a0724d1aeded8764f3e14735e844
cjunhong99 <cjunhongg99@gmail.com>
2023-09-18 00:00:45 +0800

    chore(layout): add Inter font and common boxShadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,15 @@ module.exports = {
     './libs/common-ui/src/**/*.{html,js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+      boxShadow: {
+        bottom:
+          '0 5px 6px -7px rgba(0, 0, 0, 0.6), 0 2px 4px -5px rgba(0, 0, 0, 0.06)',
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms')
