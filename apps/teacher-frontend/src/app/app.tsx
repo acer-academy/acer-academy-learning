5ffa394cd32c3df36713e704860cacdde7eb92fc
xxianhuii <keexianhui@gmail.com>
2023-09-21 00:36:05 +0800

    Create change password page but need configure updateteacher

--- apps/teacher-frontend/src/app/app.tsx
+++ apps/teacher-frontend/src/app/app.tsx
@@ -8,7 +8,8 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { TeacherAuthWrapper } from '@acer-academy-learning/common-ui';
 import TeacherAccount from '../pages/entry/TeacherAccount';
-import Profile from '../pages/profile/TeacherProfile';
+import TeacherProfile from '../pages/profile/TeacherProfile';
+import ChangePassword from '../pages/profile/ChangePassword';
 
 export function App() {
   return (
@@ -20,7 +21,8 @@ export function App() {
             <Route path="/" element={<TeacherLogin />} />
             <Route path="/sign-up" element={<TeacherSignUp />} />
             <Route path="/account" element={<TeacherAccount />} />
-            <Route path="/profile" element={<Profile />} />
+            <Route path="/profile" element={<TeacherProfile />} />
+            <Route path="/changePassword" element={<ChangePassword />} />
           </Routes>
         </ToastProvider>
       </TeacherAuthWrapper>
