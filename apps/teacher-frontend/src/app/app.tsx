a4daf902bf2557aeb1993837a7816a312422cbed
xxianhuii <keexianhui@gmail.com>
2023-09-20 22:13:04 +0800

    Create Teacher Profile Page

--- apps/teacher-frontend/src/app/app.tsx
+++ apps/teacher-frontend/src/app/app.tsx
@@ -8,6 +8,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { TeacherAuthWrapper } from '@acer-academy-learning/common-ui';
 import TeacherAccount from '../pages/entry/TeacherAccount';
+import Profile from '../pages/profile/TeacherProfile';
 
 export function App() {
   return (
@@ -19,6 +20,7 @@ export function App() {
             <Route path="/" element={<TeacherLogin />} />
             <Route path="/sign-up" element={<TeacherSignUp />} />
             <Route path="/account" element={<TeacherAccount />} />
+            <Route path="/profile" element={<Profile />} />
           </Routes>
         </ToastProvider>
       </TeacherAuthWrapper>
