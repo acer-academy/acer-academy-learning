2789ded3c4e8fb6a2b5d85d57ccfcc55ff4f2b7f
xxianhuii <keexianhui@gmail.com>
2023-09-21 17:28:54 +0800

    fixed teacher profile, edited updateTeacher api in fe

--- apps/teacher-frontend/src/pages/profile/ChangePassword.tsx
+++ apps/teacher-frontend/src/pages/profile/ChangePassword.tsx
@@ -1,88 +1,91 @@
-import { useAuth } from "@acer-academy-learning/common-ui";
-import { useState } from "react";
-import { useToast } from "@acer-academy-learning/common-ui";
-import { updateTeacher } from "../../api/teacher";
-
+import { useAuth } from '@acer-academy-learning/common-ui';
+import { useState } from 'react';
+import { useToast } from '@acer-academy-learning/common-ui';
+import { updateTeacher } from '../../api/teacher';
 
 const ChangePassword: React.FC = () => {
-    const { user } = useAuth<Teacher>();
+  const { user } = useAuth<Teacher>();
 
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
-    const { displayToast, ToastType } = useToast();
+  const { displayToast, ToastType } = useToast();
 
-    const changePassword = async () => {
-        if (password !== confirmPassword) {
-            displayToast('Password do not match', ToastType.ERROR);
-            return;
-          }
-          try {
-            // await updateTeacher(user.email, {password: password});
-            //@TODO: SET USER HERE FOR AUTH CONTEXT
-            displayToast('Password succesfully changed!', ToastType.SUCCESS);
-          } catch (error: any) {
-            displayToast(`${error}`, ToastType.ERROR);
-          }
+  const changePassword = async () => {
+    if (password !== confirmPassword) {
+      displayToast('Password do not match', ToastType.ERROR);
+      return;
     }
-
-    return (
-      <div className="h-full bg-gray-50">
-            <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Change Password
-            </h2>
-
-          <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
-            <div>
-                <span
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                    New Password
-                </span>
-                <div className="mt-2">
-                    <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    required
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                </div>
-                </div>
-
-                <div>
-                <label
-                    htmlFor="confirmPassword"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                    Reconfirm Password
-                </label>
-                <div className="mt-2">
-                    <input
-                    id="confirmPassword"
-                    name="confirmPassword"
-                    type="password"
-                    required
-                    value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                </div>
-                </div>
-                <div>
-                <button
-                    className="rounded bg-indigo-600 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    onClick={changePassword}
-                    >
-                    Change Password
-                    </button>
-                </div>
-            </div>
-        </div>
-    );
+    if (password === '') {
+      displayToast('Please enter new password', ToastType.ERROR);
+      return;
+    }
+    try {
+      await updateTeacher(user.id, { password });
+      setPassword('');
+      setConfirmPassword('');
+      //@TODO: SET USER HERE FOR AUTH CONTEXT
+      displayToast('Password succesfully changed!', ToastType.SUCCESS);
+    } catch (error: any) {
+      displayToast(`${error}`, ToastType.ERROR);
+    }
   };
 
-  export default ChangePassword;
+  return (
+    <div className="h-full bg-gray-50">
+      <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+        Change Password
+      </h2>
+
+      <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
+        <div>
+          <span className="block text-sm font-medium leading-6 text-gray-900">
+            New Password
+          </span>
+          <div className="mt-2">
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+
+        <div>
+          <label
+            htmlFor="confirmPassword"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Reconfirm Password
+          </label>
+          <div className="mt-2">
+            <input
+              id="confirmPassword"
+              name="confirmPassword"
+              type="password"
+              required
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+        <div>
+          <button
+            className="rounded bg-indigo-600 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            onClick={changePassword}
+          >
+            Change Password
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChangePassword;
--- apps/teacher-frontend/src/pages/profile/TeacherProfile.tsx
+++ apps/teacher-frontend/src/pages/profile/TeacherProfile.tsx
@@ -5,76 +5,103 @@ import { useToast } from '@acer-academy-learning/common-ui';
 import { updateTeacher } from '../../api/teacher';
 
 const TeacherProfile: React.FC = () => {
-    const { user } = useAuth<Teacher>();
-    // const user = {firstName: 'user1', lastName: 'user2', email: 'email@gmail.com', subjects: ['English', 'Math'], levels: ['P1', 'P2']}
+  const { user } = useAuth<Teacher>();
+  // const user = {firstName: 'user1', lastName: 'user2', email: 'email@gmail.com', subjects: ['English', 'Math'], levels: ['P1', 'P2']}
 
-    const [updateUser, setUpdateUser] = useState({
-        firstName: user.firstName, 
-        lastName: user.lastName, 
-        subjects: user.subjects, 
-        levels: user.levels
-    }) 
+  const [updateUser, setUpdateUser] = useState({
+    firstName: user.firstName,
+    lastName: user.lastName,
+    subjects: user.subjects,
+    levels: user.levels,
+  });
 
-    const [isEditing, setIsEditing] = useState(false)
-    const navigate = useNavigate();
+  const [isEditing, setIsEditing] = useState(false);
+  const navigate = useNavigate();
 
-    const subjects = [
-        { id: 'english', label: 'English' },
-        { id: 'science', label: 'Science' },
-        { id: 'math', label: 'Math' },
-        // add more subjects as needed
-      ];
-    
-      const levels = [
-        ['P1', 'P2', 'P3', 'P4', 'P5', 'P6'],
-        ['S1', 'S2', 'S3', 'S4', 'S5'],
-        ['J1', 'J2'],
-        // add more rows as needed
-      ];
+  enum SubjectEnum {
+    MATHEMATICS = 'MATHEMATICS',
+    ENGLISH = 'ENGLISH',
+    SCIENCE = 'SCIENCE',
+  }
 
-    const handleChange = (field: string, data: string) => {
-        if(field==='subject') {
-            setUpdateUser((prevUser) => {
-                const updatedUser = {
-                  ...prevUser,
-                  subjects: prevUser.subjects.includes(data)
-                    ? prevUser.subjects.filter((subj) => subj !== data)
-                    : [...prevUser.subjects, data],
-                };
-                return updatedUser;
-            });     
-        } else if(field==='level') {
-            setUpdateUser((prevUser) => {
-                const updatedUser = {
-                  ...prevUser,
-                  levels: prevUser.levels.includes(data)
-                    ? prevUser.levels.filter((level) => level !== data)
-                    : [...prevUser.levels, data],
-                };
-                return updatedUser;
-            });     
-        }
-    };
+  const subjects = [
+    { id: 'english', label: 'English', value: 'ENGLISH' },
+    { id: 'science', label: 'Science', value: 'SCIENCE' },
+    { id: 'math', label: 'Math', value: 'MATHEMATICS' },
+    // add more subjects as needed
+  ];
 
+  const levels = [
+    ['P1', 'P2', 'P3', 'P4', 'P5', 'P6'],
+    ['S1', 'S2', 'S3', 'S4', 'S5'],
+    ['J1', 'J2'],
+    // add more rows as needed
+  ];
 
-    const { displayToast, ToastType } = useToast();
-
-    const editProfile = () => {
-      setIsEditing(true)
+  const handleChange = (field: string, data: string) => {
+    if (field === 'subject') {
+      setUpdateUser((prevUser) => {
+        const updatedUser = {
+          ...prevUser,
+          subjects: prevUser.subjects.includes(data)
+            ? prevUser.subjects.filter((subj) => subj !== data)
+            : [...prevUser.subjects, data],
+        };
+        return updatedUser;
+      });
+    } else if (field === 'level') {
+      setUpdateUser((prevUser) => {
+        const updatedUser = {
+          ...prevUser,
+          levels: prevUser.levels.includes(data)
+            ? prevUser.levels.filter((level) => level !== data)
+            : [...prevUser.levels, data],
+        };
+        return updatedUser;
+      });
     }
+  };
 
-    const saveProfile = async () => {
-      try {
-        console.log(updateUser)
-        // await updateTeacher(user.email, {...updateUser})
-        //see if need refresh auth after update 
-        setIsEditing(false)
-        displayToast(`Profile updated!`, ToastType.SUCCESS)
-      } catch(error) {
-        displayToast('Profile update failed', ToastType.ERROR);
+  const { displayToast, ToastType } = useToast();
+
+  const editProfile = () => {
+    setIsEditing(true);
+  };
+
+  const saveProfile = async () => {
+    try {
+      if (updateUser.subjects.length === 0 || updateUser.levels.length === 0) {
+        displayToast(
+          `You need to choose at least a Subject and Level`,
+          ToastType.ERROR,
+        );
+      } else {
+        const subjectEnumArray: SubjectEnum[] = updateUser.subjects
+          .filter((subject) => subject in SubjectEnum)
+          .map((subject) => SubjectEnum[subject as keyof typeof SubjectEnum]);
+        await updateTeacher(user.id, {
+          ...updateUser,
+          subjects: subjectEnumArray,
+        });
+        //see if need refresh auth after update
+        setIsEditing(false);
+        displayToast(`Profile updated!`, ToastType.SUCCESS);
       }
+    } catch (error) {
+      displayToast('Profile update failed', ToastType.ERROR);
     }
+  };
 
+  const displaySubjects = () => {
+    let display = [];
+    for (let i = 0; i < updateUser.subjects.length; i++) {
+      display.push(
+        updateUser.subjects[i].charAt(0).toUpperCase() +
+          updateUser.subjects[i].slice(1),
+      );
+    }
+    return display.join(', ');
+  };
 
   return (
     <div className="h-full bg-gray-50">
@@ -87,66 +114,78 @@ const TeacherProfile: React.FC = () => {
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[1000px]">
           <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
-            { isEditing ? (
-            <button
-            onClick={saveProfile}
-            className="justify-end rounded bg-indigo-600 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Save Profile
-            </button>
-            ):(
-            <button
-            onClick={editProfile}
-            className="justify-end rounded bg-indigo-600 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Edit Profile
-            </button>
-          )}
+            {isEditing ? (
+              <button
+                onClick={saveProfile}
+                className="justify-end rounded bg-indigo-600 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Save Profile
+              </button>
+            ) : (
+              <button
+                onClick={editProfile}
+                className="justify-end rounded bg-indigo-600 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Edit Profile
+              </button>
+            )}
             <div className="space-y-2">
               <div className="flex justify-between items-center">
                 <span className="font-semibold text-gray-600">First Name:</span>
-                { isEditing ? (
-                    <input
+                {isEditing ? (
+                  <input
                     type="text"
                     value={updateUser.firstName}
-                    onChange = {(e: React.ChangeEvent<HTMLInputElement>) => setUpdateUser({...updateUser, firstName:e.target.value})}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                      setUpdateUser({
+                        ...updateUser,
+                        firstName: e.target.value,
+                      })
+                    }
                     autoFocus
-                    />
-                ): 
-                (
-                <span className="text-gray-800">{updateUser.firstName}</span>
+                  />
+                ) : (
+                  <span className="text-gray-800">{updateUser.firstName}</span>
                 )}
               </div>
               <div className="flex justify-between items-center">
                 <span className="font-semibold text-gray-600">Last Name:</span>
-                { isEditing ? (
-                    <input
+                {isEditing ? (
+                  <input
                     type="text"
                     value={updateUser.lastName}
-                    onChange = {(e: React.ChangeEvent<HTMLInputElement>) => setUpdateUser({...updateUser, lastName:e.target.value})}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                      setUpdateUser({ ...updateUser, lastName: e.target.value })
+                    }
                     autoFocus
-                    />
-                ): 
-                (
-                <span className="ext-gray-800">{updateUser.lastName}</span>
+                  />
+                ) : (
+                  <span className="ext-gray-800">{updateUser.lastName}</span>
                 )}
               </div>
               <div className="flex justify-between items-center">
-                <span className="font-semibold text-gray-600">Change Password:</span>
+                <span className="font-semibold text-gray-600">
+                  Change Password:
+                </span>
                 <button
-                onClick={() => { navigate('/changePassword')}}
-                className="rounded bg-indigo-600 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  onClick={() => {
+                    navigate('/changePassword');
+                  }}
+                  className="rounded bg-indigo-600 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Change Password
                 </button>
               </div>
               <div>
-              <div className="flex justify-between items-center">
-                <span className="font-semibold text-gray-600">Levels:</span>
-                    { isEditing ? (
+                <div className="flex justify-between items-center">
+                  <span className="font-semibold text-gray-600">Levels:</span>
+                  {isEditing ? (
                     <div className="w-3/4 mt-2 flex flex-col space-y-4 pl-3 py-5">
                       {levels.map((row, rowIndex) => (
-                        <div key={rowIndex} className="flex space-x-4 justify-start">
+                        <div
+                          key={rowIndex}
+                          className="flex space-x-4 justify-start"
+                        >
                           {row.map((level) => (
                             <label key={level} className="flex items-center">
                               <input
@@ -155,50 +194,59 @@ const TeacherProfile: React.FC = () => {
                                 onChange={() => handleChange('level', level)}
                                 className="h-5 w-5 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
                               />
-                              <span className="ml-2 text-gray-700">{level}</span>
+                              <span className="ml-2 text-gray-700">
+                                {level}
+                              </span>
                             </label>
                           ))}
                         </div>
                       ))}
                     </div>
-                      ) : (
-                      <span className="ext-gray-800">{updateUser.levels.join(', ')}</span>
-                      )
-                    } 
+                  ) : (
+                    <span className="ext-gray-800">
+                      {updateUser.levels.join(', ')}
+                    </span>
+                  )}
                 </div>
                 <div className="flex justify-between items-center">
-                <span className="font-semibold text-gray-600">Subjects:</span>
-                {isEditing ? (
+                  <span className="font-semibold text-gray-600">Subjects:</span>
+                  {isEditing ? (
                     <div className="w-3/4 mt-2 flex justify-between space-y-4 pl-3">
                       {subjects.map((subject) => (
                         <label key={subject.id} className="flex items-center">
                           <input
                             type="checkbox"
-                            checked={updateUser.subjects.includes(subject.label)}
-                            onChange={() => handleChange('subject', subject.label)}
+                            checked={updateUser.subjects.includes(
+                              subject.value,
+                            )}
+                            onChange={() =>
+                              handleChange('subject', subject.value)
+                            }
                             className="h-5 w-5 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                            />
-                          <span className="ml-2 text-gray-700">{subject.label}</span>
+                          />
+                          <span className="ml-2 text-gray-700">
+                            {subject.value}
+                          </span>
                         </label>
                       ))}
                     </div>
-                ) : (
-                <span className="text-gray-800">{updateUser.subjects.join(', ')}</span>
-                )}
+                  ) : (
+                    <span className="text-gray-800">
+                      {updateUser.subjects.join(', ')}
+                    </span>
+                  )}
                 </div>
-
+              </div>
             </div>
-              </div>
-              <div className="flex justify-between items-center">
-                <span className="font-semibold text-gray-600">Email:</span>
-                <span className="text-gray-800">{user.email}</span>
-              </div>
+            <div className="flex justify-between items-center">
+              <span className="font-semibold text-gray-600">Email:</span>
+              <span className="text-gray-800">{user.email}</span>
             </div>
           </div>
         </div>
       </div>
+    </div>
   );
 };
-
 
 export default TeacherProfile;
