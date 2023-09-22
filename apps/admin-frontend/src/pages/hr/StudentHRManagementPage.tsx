ddd175a99e99b4cad9a2cc6839342eb521362826
Lyn <lyntan2001@outlook.com>
2023-09-23 02:45:54 +0800

    Fix minor bugs.

--- apps/admin-frontend/src/pages/hr/StudentHRManagementPage.tsx
+++ apps/admin-frontend/src/pages/hr/StudentHRManagementPage.tsx
@@ -46,6 +46,7 @@ export const StudentHRManagementPage: React.FC = () => {
         <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
           <button
             type="button"
+            onClick={() => setIsAddWhitelistModalOpen(true)}
             className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Add whitelist
