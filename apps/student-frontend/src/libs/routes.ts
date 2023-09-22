59446c5ca52536046a247eb2745bc43bc4ec298c
keithcharlesc <keithcharleschan@gmail.com>
2023-09-22 16:30:30 +0800

    Fixed routing for student forgot password , reset password and added back navigations

--- apps/student-frontend/src/libs/routes.ts
+++ apps/student-frontend/src/libs/routes.ts
@@ -1,6 +1,8 @@
 // Auth routes
 export const LOGIN = '/login';
 export const SIGN_UP = '/sign-up';
+export const FORGOT_PASSWORD = '/forgot-password';
+export const RESET_PASSWORD = '/reset-password';
 
 // Dashboard routes
 export const DASHBOARD = '/';
