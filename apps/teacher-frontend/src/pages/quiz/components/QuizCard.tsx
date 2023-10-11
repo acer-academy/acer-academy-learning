3aefadcd50a98a33927e759213b3ae3427c78336
cjunhong99 <cjunhongg99@gmail.com>
2023-10-11 19:39:27 +0800

    feat(quiz): add boilerplate for CreateQuiz

---
+++ apps/teacher-frontend/src/pages/quiz/components/QuizCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const QuizCard = () => {
+  return (
+    <form className="border-b border-gray-200 bg-white px-4 py-5 sm:px-6 rounded shadow space-y-4 flex flex-col">
+      {/* title */}
+      {/* description */}
+      {/* total marks */}
+      {/* reward points */}
+      {/* time allowed */}
+    </form>
+  );
+};
