67f2d94392b13eb388e068d6ce9198f92ec33401
Lyn <lyntan2001@outlook.com>
2023-10-16 17:26:48 +0800

    Add options for question selection mode.

---
+++ apps/teacher-frontend/src/pages/quiz/components/FindQuestionsButton.tsx
@@ -0,0 +1,37 @@
+interface FindQuestionsButtonProps {
+  setIsQuestionBankModalOpen: (isOpen: boolean) => void;
+}
+
+export const FindQuestionsButton: React.FC<FindQuestionsButtonProps> = (
+  props: FindQuestionsButtonProps,
+) => {
+  const { setIsQuestionBankModalOpen } = props;
+
+  return (
+    <div className="flex align-middle justify-between">
+      <div className="flex align-middle gap-4"></div>
+      <button
+        className="inline-flex justify-center px-4 py-2 text-white bg-teacherBlue-500 border border-transparent rounded-md hover:bg-teacherBlue-700"
+        onClick={() => {
+          setIsQuestionBankModalOpen(true);
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="w-6 h-6 mr-3"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+          />
+        </svg>
+        Find Questions
+      </button>
+    </div>
+  );
+};
--- apps/teacher-frontend/src/pages/quiz/components/QuestionsSection.tsx
+++ apps/teacher-frontend/src/pages/quiz/components/QuestionsSection.tsx
@@ -1,15 +1,16 @@
-import {
-  QuizQuestionInQuizType,
-  getQuizQuestionById,
-} from '@acer-academy-learning/data-access';
+import { QuizQuestionInQuizType } from '@acer-academy-learning/data-access';
 import { useEffect, useState } from 'react';
 import { SelectedQuestionsTable } from './SelectedQuestionsTable';
 import QuestionBankModal from './QuestionBankModal';
 import { useToast } from '@acer-academy-learning/common-ui';
+import { FindQuestionsButton } from './FindQuestionsButton';
+import { QuestionSelectionModeRadio } from './QuestionSelectionModeRadio';
 
 export const QuestionsSection = () => {
   const { displayToast, ToastType } = useToast();
 
+  const [questionSelectionMode, setQuestionSelectionMode] =
+    useState<string>('');
   const [selectedQuestions, setSelectedQuestions] = useState<
     QuizQuestionInQuizType[]
   >([]);
@@ -38,41 +39,38 @@ export const QuestionsSection = () => {
 
   return (
     <>
-      <div className="flex align-middle justify-between">
-        <div className="flex align-middle gap-4"></div>
-        <button
-          className="inline-flex justify-center px-4 py-2 text-white bg-teacherBlue-500 border border-transparent rounded-md hover:bg-teacherBlue-700"
-          onClick={() => {
-            setIsQuestionBankModalOpen(true);
-          }}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            className="w-6 h-6 mr-3"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-            />
-          </svg>
-          Find Questions
-        </button>
-      </div>
-      <QuestionBankModal
-        open={isQuestionBankModalOpen}
-        setOpen={setIsQuestionBankModalOpen}
-        selectedQuestions={selectedQuestions}
-        onClick={handleAddQuestions}
-      />
-      <SelectedQuestionsTable
-        selectedQuestions={selectedQuestions}
-        editQuestionMarks={editQuestionMarks}
-      />
+      {questionSelectionMode === '' ? (
+        <QuestionSelectionModeRadio
+          setQuestionSelectionMode={setQuestionSelectionMode}
+        />
+      ) : questionSelectionMode === 'MANUAL_SELECTION' ? (
+        <div className="flex justify-center text-3xl font-bold pt-5">
+          Manual Selection
+        </div>
+      ) : (
+        <div className="flex justify-center text-3xl font-bold pt-5">
+          Auto-Generate
+        </div>
+      )}
+      {questionSelectionMode === 'MANUAL_SELECTION' && (
+        <>
+          <FindQuestionsButton
+            setIsQuestionBankModalOpen={setIsQuestionBankModalOpen}
+          />
+          <QuestionBankModal
+            open={isQuestionBankModalOpen}
+            setOpen={setIsQuestionBankModalOpen}
+            selectedQuestions={selectedQuestions}
+            onClick={handleAddQuestions}
+          />
+        </>
+      )}
+      {questionSelectionMode !== '' && (
+        <SelectedQuestionsTable
+          selectedQuestions={selectedQuestions}
+          editQuestionMarks={editQuestionMarks}
+        />
+      )}
     </>
   );
 };
