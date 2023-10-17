19f47a800ac5d89e98737e523d6376aaba8d1efc
keithcharlesc <keithcharleschan@gmail.com>
2023-10-17 18:35:02 +0800

    Calendar custom event

--- apps/teacher-frontend/src/pages/calendar/TestCalendar.tsx
+++ apps/teacher-frontend/src/pages/calendar/TestCalendar.tsx
@@ -10,7 +10,7 @@ import { EventItem } from './CustomCalendar.types';
 import './index.css';
 import SessionEvent from './SessionEvent';
 import { SessionData } from 'libs/data-access/src/lib/types/session';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import YearView from './YearView/YearView';
 
 const localizer = momentLocalizer(moment);
@@ -33,61 +33,88 @@ interface CalendarProps {
   }[];
 }
 
+// const getComponents = (currentView: string) => ({
+//   event: ({ event }: { event: any }) => {
+//     console.log(currentView);
+
+//     const data = event?.data;
+//     if (currentView === 'month') {
+//       return <p>hello</p>;
+//     }
+
+//     return <SessionEvent session={data?.session} onDoubleClick={() => {}} />;
+//   },
+// });
+
 export const TestCalendar = ({
   onShowSessionView,
   sessionsData,
 }: CalendarProps) => {
-  const [view, setView] = useState(Views.WEEK);
+  const [view, setView] = useState('week');
   const [date, setDate] = useState(new Date());
+
+  const viewRef = useRef(view); // create a ref for the view
+
+  // update the ref every time the view changes
+  useEffect(() => {
+    viewRef.current = view;
+  }, [view]);
 
   const components = {
     event: ({ event }: { event: any }) => {
-      const data = event?.data;
-      if (data?.session)
-        return (
-          <SessionEvent session={data?.session} onDoubleClick={() => {}} />
-        );
+      console.log(viewRef.current); // log the current value from the ref
 
-      return null;
+      const data = event?.data;
+      if (viewRef.current === 'month') {
+        // use the ref value here
+        return <p>hello</p>;
+      }
+
+      return <SessionEvent session={data?.session} onDoubleClick={() => {}} />;
     },
   };
 
   return (
-    <DndCalendar
-      onSelectSlot={({ start, end }) => {
-        onShowSessionView({ start, end });
-      }}
-      onDoubleClickEvent={(event) => {
-        const session = event?.data?.session;
-        session && onShowSessionView(session);
-      }}
-      events={sessionsData}
-      style={{
-        width: '100%',
-        height: '100%',
-        background: 'white',
-        padding: 20,
-      }}
-      components={components}
-      selectable
-      // defaultView={Views.WEEK}
-      view={view}
-      date={date}
-      onView={(view) => setView(view)}
-      onNavigate={(date) => setDate(date)}
-      formats={{
-        dayHeaderFormat: (date) => moment(date).format('Do MMMM YYYY'),
-      }}
-      views={
-        {
-          month: true,
-          day: true,
-          week: true,
-          year: YearView,
-        } as any
-      }
-      messages={{ year: 'Year' } as any}
-      {...initProps}
-    />
+    <>
+      View: {view}
+      <DndCalendar
+        onSelectSlot={({ start, end }) => {
+          onShowSessionView({ start, end });
+        }}
+        onDoubleClickEvent={(event) => {
+          const session = event?.data?.session;
+          session && onShowSessionView(session);
+        }}
+        events={sessionsData}
+        style={{
+          width: '100%',
+          height: '100%',
+          background: 'white',
+          padding: 20,
+        }}
+        components={components}
+        selectable
+        // defaultView={Views.WEEK}
+        view={view}
+        // onView={setView} // Corrected this line
+        date={date}
+        onView={(view) => setView(view)}
+        onNavigate={(date) => setDate(date)}
+        formats={{
+          dayHeaderFormat: (date) => moment(date).format('Do MMMM YYYY'),
+        }}
+        views={
+          {
+            month: true,
+            day: true,
+            week: true,
+            agenda: true,
+            year: YearView,
+          } as any
+        }
+        messages={{ year: 'Year' } as any}
+        {...initProps}
+      />
+    </>
   );
 };
