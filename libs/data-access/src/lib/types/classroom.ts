64f849400b353cce854309f10be301d7d0cae949
keithcharlesc <keithcharleschan@gmail.com>
2023-10-19 20:33:04 +0800

    Added centre under classroom under Session

--- libs/data-access/src/lib/types/classroom.ts
+++ libs/data-access/src/lib/types/classroom.ts
@@ -1,9 +1,12 @@
+import { CentreData } from "./centre";
+
 export interface ClassroomData {
   id: string;
   name: string;
   available: boolean;
   capacity: number;
   centreId: string;
+  centre: CentreData;
 }
 
 export interface ClassroomCreateData {
