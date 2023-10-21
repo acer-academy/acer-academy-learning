03d2cbbe42fecebecfcec7d93192d46446cb5f1d
cjunhong99 <cjunhongg99@gmail.com>
2023-10-21 15:50:15 +0800

    fix(quiz-ui): add topics and levels fields for CreateQuiz and UpdateQuiz

--- apps/teacher-frontend/src/pages/quiz/components/QuestionsSection.tsx
+++ apps/teacher-frontend/src/pages/quiz/components/QuestionsSection.tsx
@@ -1,4 +1,8 @@
-import { QuizQuestionInQuizType } from '@acer-academy-learning/data-access';
+import {
+  CreateQuizType,
+  LevelEnum,
+  QuizQuestionInQuizType,
+} from '@acer-academy-learning/data-access';
 import { useState } from 'react';
 import { SelectedQuestionsTable } from './SelectedQuestionsTable';
 import QuestionBankModal from './QuestionBankModal';
@@ -8,6 +12,14 @@ import { AutoGenerateInputParametersForm } from './AutoGenerateInputParametersFo
 import { QuizRewardMinimumMarksField } from './QuizRewardMinimumMarksField';
 import { QuizRewardPointsField } from './QuizRewardPointsField';
 import { QuizTimeAllowedField } from './QuizTimeAllowedField';
+import {
+  ErrorField,
+  GenericBadges,
+  GenericComboBox,
+  screamingSnakeToTitleCase,
+} from '@acer-academy-learning/common-ui';
+import { Controller, useFormContext } from 'react-hook-form';
+import { ErrorMessage } from '@hookform/error-message';
 interface QuestionsSectionProps {
   selectedQuestions: QuizQuestionInQuizType[];
   setSelectedQuestions: (selectedQuestions: QuizQuestionInQuizType[]) => void;
@@ -15,9 +27,17 @@ interface QuestionsSectionProps {
   setQuestionSelectionMode: (questionSelectionMode: string) => void;
 }
 
+const levelEnums = Object.values(LevelEnum);
+
 export const QuestionsSection: React.FC<QuestionsSectionProps> = (
   props: QuestionsSectionProps,
 ) => {
+  const {
+    watch,
+    control,
+    formState: { errors },
+  } = useFormContext<CreateQuizType>();
+  const watchTopics = watch('topics');
   const {
     selectedQuestions,
     setSelectedQuestions,
@@ -71,6 +91,42 @@ export const QuestionsSection: React.FC<QuestionsSectionProps> = (
           selectedQuestions={selectedQuestions}
           setSelectedQuestions={setSelectedQuestions}
         />
+        <div>
+          <p className="text-base font-bold">Topics: </p>
+          <GenericBadges
+            badges={watchTopics}
+            getDisplayValue={(topic) => screamingSnakeToTitleCase(topic)}
+          />
+        </div>
+        <div>
+          <p className="text-base font-bold">Levels: </p>
+          <Controller
+            control={control}
+            name="levels"
+            render={({ field: { onChange, value } }) => (
+              <>
+                <GenericBadges
+                  onChange={onChange}
+                  badges={value}
+                  getDisplayValue={(level) => screamingSnakeToTitleCase(level)}
+                  allowRemove
+                />
+                <ErrorMessage
+                  errors={errors}
+                  name="levels"
+                  render={({ message }) => <ErrorField message={message} />}
+                />
+                <GenericComboBox
+                  options={levelEnums}
+                  onChange={onChange}
+                  selected={value}
+                  displayValue={(level) => screamingSnakeToTitleCase(level)}
+                  containerStyle="w-[50%]"
+                />
+              </>
+            )}
+          />
+        </div>
         <div className="grid grid-cols-2 gap-4 w-[70%]">
           <QuizTimeAllowedField />
           <QuizRewardMinimumMarksField />
